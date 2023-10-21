686308199a31ba5702ba7b4cbc0ae7d36dffa819
LBD <lyanbasora@gmail.com>
2023-10-20 22:48:48 -0400

    Add createGrid() function and button click event

--- etch-project.js
+++ etch-project.js
@@ -1,34 +1,41 @@
 // create children of container based on user input
-let container = document.querySelector('.grid-container');
-let gridSize = 100; // = input from user
-// create grid function for later use
-let rowContainer;
-//loop for creation of grid
-for (let i = 0; i < gridSize; i++) {
-    rowContainer = document.createElement('div');
-    rowContainer.className = 'row-container';
-    container.appendChild(rowContainer);
-    // second loop for creation of children boxes of rowSquare
-    let childSquare;
-    for (let j = 0; j < gridSize; j++) {
-        childSquare = document.createElement('div');
-        childSquare.className = 'square';
-        childSquare.addEventListener('mouseover', (event) => {
-            let target = event.target;
-            target.setAttribute('style', 
-            'background-color: yellow;')});
-        rowContainer.appendChild(childSquare);
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
     }
 }
 
-let squares = document.body.querySelectorAll('.square')
+function createGrid() {
 
-// container.addEventListener('mouseover', (event) => {
-//     let target = event.target;
-//     target.setAttribute('style', 'background-color: yellow;');
-// })
-
-let hoverListener = function() {
-    // if hover, change background color
+    let container = document.querySelector('.grid-container');
+    removeAllChildNodes(container);
+    userInput = prompt()
+    if (userInput > 100) {
+    alert('Input size above limit of 100')
+    } else {
+        let gridSize = userInput; // = input from user
+        // create grid function for later use
+        let rowContainer;
+        //loop for creation of grid
+        for (let i = 0; i < gridSize; i++) {
+        rowContainer = document.createElement('div');
+        rowContainer.className = 'row-container';
+        container.appendChild(rowContainer);
+        // second loop for creation of children boxes of rowSquare
+        let childSquare;
+        for (let j = 0; j < gridSize; j++) {
+            childSquare = document.createElement('div');
+            childSquare.className = 'square';
+            childSquare.addEventListener('mouseover', (event) => {
+                let target = event.target;
+                target.setAttribute('style', 
+                'background-color: yellow;')});
+            rowContainer.appendChild(childSquare);
+        }}
+    }
 }
 
+    let btn = document.querySelector('#set-size')
+    btn.addEventListener('click', (event) => {
+        createGrid();
+    })
